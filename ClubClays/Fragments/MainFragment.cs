692a75d9fc061f62ef24191052c2cdb73f18fcbb
Euan Deas <euandeas@hotmail.com>
2020-07-30 17:22:21 +0100

    Setting up home screen basics

---
+++ ClubClays/Fragments/MainFragment.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Fragment = AndroidX.Fragment.App.Fragment;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+
+namespace ClubClays.Fragments
+{
+    public class MainFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // non-graphical initialisations (you can assign variables, get Intent extras, and anything else that doesn't involve the View hierarchy)
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // do any graphical initialisations (e.g. You can assign your View variables)
+            View view = inflater.Inflate(Resource.Layout.fragment_main, container, false);
+
+            ((AppCompatActivity)Activity).SetSupportActionBar(view.FindViewById<Toolbar>(Resource.Id.toolbar));
+            ((AppCompatActivity)Activity).SupportActionBar.Title = "test";
+
+            return view;
+        }
+    }
+}
--- ClubClays/MainActivity.cs
+++ ClubClays/MainActivity.cs
@@ -1,20 +1,31 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Support.V7.App;
 using Android.Runtime;
-using Android.Widget;
+using AndroidX.AppCompat.App;
+using FragmentTransaction = AndroidX.Fragment.App.FragmentTransaction;
+using ClubClays.Fragments;
 
 namespace ClubClays
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/SplashTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private MainFragment mainFragment = new MainFragment();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            SetTheme(Resource.Style.AppTheme);
             base.OnCreate(savedInstanceState);
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            if (savedInstanceState == null)
+            {
+                FragmentTransaction fragmentTx = SupportFragmentManager.BeginTransaction();
+                fragmentTx.Replace(Resource.Id.container, mainFragment);
+                fragmentTx.Commit();
+            }
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
