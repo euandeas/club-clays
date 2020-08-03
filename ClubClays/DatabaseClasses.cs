702947753012c43d3ce547060e91635d391b8dcf
Euan Deas <euandeas@hotmail.com>
2020-08-03 15:10:45 +0100

    Minor changes to some layouts and basics of database

---
+++ ClubClays/DatabaseClasses.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace ClubClays
+{
+    //Main tables
+    public class Shoots
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string EventType { get; set; }
+        public int NumStands { get; set; }
+        public int ClayAmount { get; set; }
+        public int StartingStand { get; set; }
+        public string DataCollectionType { get; set; }
+        [MaxLength(250)]
+        public string Notes { get; set; }
+    }
+
+    public class Stands
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [ForeignKey]
+        public string Symbol { get; set; }
+    }
+
+    public class StandScores
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+    }
+
+    public class Shots
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+    }
+    public class OverallScores
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+    }
+    public class Shooters
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+    }
+
+    //Shoot Format tables
+    public class ShootFormats
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+    }
+    public class StandFormats
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+    }
+}
