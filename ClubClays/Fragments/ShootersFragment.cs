4e66fe0568cfa455c0c3b9d94dfc365acca6e840
Euan Deas <euandeas@hotmail.com>
2020-10-05 20:59:36 +0100

    Started implementing shooters fragment

---
+++ ClubClays/Fragments/ShootersFragment.cs
@@ -0,0 +1,24 @@
+﻿using Android.OS;
+using Android.Views;
+using Fragment = AndroidX.Fragment.App.Fragment;
+
+namespace ClubClays.Fragments
+{
+    public class ShootersFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            View view = inflater.Inflate(Resource.Layout.fragment_shooters, container, false);
+
+            return view;
+        }
+    }
+}
