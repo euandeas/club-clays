536526a492b8d1c15d8aa4f4b4f6af7e33c4cddf
Euan Deas <euandeasapps@gmail.com>
2021-02-04 15:11:23 +0000

    Set up the hit miss buttons

--- ClubClays/Fragments/ScoreTakingFragment.cs
+++ ClubClays/Fragments/ScoreTakingFragment.cs
@@ -12,6 +12,10 @@ namespace ClubClays.Fragments
     {
         private ShootScoreManagement scoreManagementModel;
         private string trackingType;
+
+        int shot1Val = 0;
+        int shot2Val = 0;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -41,9 +45,62 @@ namespace ClubClays.Fragments
             pairNumLabel.Text = $"Pair: {scoreManagementModel.CurrentPair}";
             currentStandScoreLabel.Text = $"Current Score: {scoreManagementModel.CurrentStandScore}";
 
+            Button shot1Button = view.FindViewById<Button>(Resource.Id.button1);
+            Button shot2Button = view.FindViewById<Button>(Resource.Id.button2);
 
+            shot1Button.Click += Shot1Button_Click;
+            shot2Button.Click += Shot2Button_Click;
+
+            TextView nextButton = view.FindViewById<TextView>(Resource.Id.nextButton);
+            nextButton.Click += NextButton_Click;
 
             return view;
+        }
+
+        private void NextButton_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Shot1Button_Click(object sender, System.EventArgs e)
+        {
+            ShotButton_Click(sender as Button, ref shot1Val);
+        }
+
+        private void Shot2Button_Click(object sender, System.EventArgs e)
+        {
+            ShotButton_Click(sender as Button, ref shot2Val);
+        }
+
+        private void ShotButton_Click(Button sender, ref int shotCounter)
+        {
+            if(shotCounter == 2)
+            {
+                shotCounter = 0;
+            }
+            else
+            {
+                shotCounter++;
+            }
+
+            switch (shotCounter)
+            {
+                case 0:
+                    sender.Text = "";
+                    sender.SetBackgroundResource(Resource.Drawable.default_hit_miss_button);
+                    break;
+
+                case 1:
+                    sender.Text = "Hit";
+                    sender.SetBackgroundResource(Resource.Drawable.hit_hit_miss_button);
+                    break;
+
+                case 2:
+                    sender.Text = "Miss";
+                    sender.SetBackgroundResource(Resource.Drawable.miss_hit_miss_button);
+                    break;
+                      
+            }
         }
     }
 }
