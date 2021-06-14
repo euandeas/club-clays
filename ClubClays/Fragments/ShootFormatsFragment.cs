a51717854ad5cc595c86f69edc079994fbd52b7f
Euan Deas <euandeasapps@gmail.com>
2021-06-14 16:45:49 +0100

    Add fragments and layouts for shoot formats

--- ClubClays/Fragments/ShootFormatsFragment.cs
+++ ClubClays/Fragments/ShootFormatsFragment.cs
@@ -1,10 +1,19 @@
-﻿using Android.OS;
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
 using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Fragment = AndroidX.Fragment.App.Fragment;
 
 namespace ClubClays.Fragments
 {
-    public class StandFormatsManagmentFragment : Fragment
+    public class ShootFormatsFragment : Fragment
     {
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -16,7 +25,7 @@ namespace ClubClays.Fragments
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
-            View view = inflater.Inflate(Resource.Layout.fragment_shooters, container, false);
+            View view = inflater.Inflate(Resource.Layout.fragment_shoot_formats, container, false);
 
             return view;
         }
