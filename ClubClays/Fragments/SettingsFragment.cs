375cc70d65f5b4390b647a4606c446f32f812ce0
Euan Deas <33450935+euandeas@users.noreply.github.com>
2022-08-05 20:18:08 +0100

    Implemented basics

---
+++ ClubClays/Fragments/SettingsFragment.cs
@@ -0,0 +1,52 @@
+﻿using Android.OS;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using AndroidX.Preference;
+using Google.Android.Material.AppBar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubClays.Fragments
+{
+    public class SettingsFragment : PreferenceFragmentCompat
+    {
+        public override void OnCreatePreferences(Bundle? savedInstanceState, string? rootKey)
+        {
+            SetPreferencesFromResource(Resource.Xml.preferences, rootKey);
+            ListPreference theme = (ListPreference)FindPreference("theme_preference")!;
+
+            theme.PreferenceChange += Theme_PreferenceChange;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup? container, Bundle? savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+
+            View view = base.OnCreateView(inflater, container, savedInstanceState)!;
+
+            //MaterialToolbar toolbar = view.FindViewById<MaterialToolbar>(Resource.Id.toolbar)!;
+            //(Activity as MainActivity)!.SetupToolBar(toolbar);
+
+            return view;
+        }
+
+        private void Theme_PreferenceChange(object? sender, Preference.PreferenceChangeEventArgs? e)
+        {
+            switch (e!.NewValue!.ToString())
+            {
+                case "Light":
+                    ((AppCompatActivity)Activity!).Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightNo);
+                    break;
+                case "Dark":
+                    ((AppCompatActivity)Activity!).Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightYes);
+                    break;
+                case "System Default":
+                    ((AppCompatActivity)Activity!).Delegate.SetLocalNightMode((Build.VERSION.SdkInt >= BuildVersionCodes.Q) ? AppCompatDelegate.ModeNightFollowSystem : AppCompatDelegate.ModeNightAutoBattery);
+                    break;
+            }
+        }
+    }
+}
--- ClubClays/MainActivity.cs
+++ ClubClays/MainActivity.cs
@@ -1,13 +1,40 @@
+using Android.Content;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using AndroidX.Core.View;
+using AndroidX.Preference;
+using SplashScreenX = AndroidX.Core.SplashScreen.SplashScreen;
+
 namespace ClubClays
 {
-    [Activity(Label = "@string/app_name", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.App.Starting", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle? savedInstanceState)
         {
+            if (savedInstanceState == null)
+            {
+                ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(Application.Context)!;
+                switch (prefs.GetString("theme_preference", "System Default"))
+                {
+                    case "Light":
+                        base.Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightNo);
+                        break;
+                    case "Dark":
+                        base.Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightYes);
+                        break;
+                    case "System Default":
+                        base.Delegate.SetLocalNightMode((Build.VERSION.SdkInt >= BuildVersionCodes.Q) ? AppCompatDelegate.ModeNightFollowSystem : AppCompatDelegate.ModeNightAutoBattery);
+                        break;
+                }
+            }
+
+            SplashScreenX.InstallSplashScreen(this);
+
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
+            WindowCompat.SetDecorFitsSystemWindows(Window!, false);
+
             SetContentView(Resource.Layout.activity_main);
         }
     }
