c0144a490c68e2b37e8b34a9e4017f2c674dabfa
Euan Deas <33450935+euandeas@users.noreply.github.com>
2022-08-22 23:40:19 +0100

    Preparation

--- ClubClays/DatabaseModels.cs
+++ ClubClays/DatabaseModels.cs
@@ -1,6 +1,70 @@
-﻿
+﻿namespace ClubClays.DatabaseModels;
 
-namespace ClubClays.DatabaseModels
+using SQLite;
+
+/// <summary>
+/// The main database tables for storing shoot data
+/// </summary>
+public class Shooter
 {
-    
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public bool PrimaryUser { get; set; }
+    public string Name { get; set; }
+    public string Class { get; set; }
 }
+
+public class Shoot
+{
+
+}
+
+public class Round
+{
+
+}
+
+
+public class StandFormat
+{
+
+}
+
+public class StandClayFormat
+{
+
+}
+
+public class OverallScore
+{
+
+}
+
+public class StandScore
+{
+
+}
+
+public class Shots
+{
+
+}
+
+/// <summary>
+/// The tables used for saving formats that can be used again
+/// </summary>
+public class SavedRoundFormat
+{
+
+}
+
+public class SavedStandFormat 
+{ 
+
+}
+
+public class SavedClayFormat
+{
+
+}
+
